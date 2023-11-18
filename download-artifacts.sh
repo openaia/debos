b86144eb0ebacd437f95b055ad9500a190049185
Jagan Teki <jagan@edgeble.ai>
2023-11-18 23:54:57 +0530

    download-artifacts: Add modules
    Signed-off-by: Jagan Teki <jagan@edgeble.ai>

--- download-artifacts.sh
+++ download-artifacts.sh
@@ -5,21 +5,25 @@ if [ -d "prebuilt" ] ; then
     exit 1
 fi
 
-U_BOOT_REL="ncm6b-v2023.11"
-
 # u-boot
+U_BOOT_REL="ncm6b-v2023.11"
 mkdir -p prebuilt
 echo "=== Downloading u-boot artifacts ==="
 wget --no-verbose --show-progress -P prebuilt "https://github.com/openaia/u-boot/releases/download/$U_BOOT_REL/idbloader.img"
 wget --no-verbose --show-progress -P prebuilt "https://github.com/openaia/u-boot/releases/download/$U_BOOT_REL/u-boot.itb"
 
-KERNEL_REL="v2023.11-6TOPS"
-
 # linux
+KERNEL_REL="v2023.11-6TOPS"
 echo "=== Downloading linux artifacts ==="
 wget --no-verbose --show-progress -P prebuilt "https://github.com/openaia/kernel/releases/download/$KERNEL_REL/linux.tar.xz"
 tar xvf prebuilt/linux.tar.xz -C prebuilt
 rm -rf prebuilt/linux.tar.xz
+
+# modules
+echo "=== Downloading modules artifacts ==="
+wget --no-verbose --show-progress -P prebuilt "https://github.com/openaia/kernel/releases/download/$KERNEL_REL/modules.tar.xz"
+tar xvf prebuilt/modules.tar.xz -C prebuilt
+rm -rf prebuilt/modules.tar.xz
 
 find prebuilt/
 echo "=== Remember to remove prebuilt directory for new aritfacts to load ==="
