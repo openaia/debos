33519263484aae345d4cd56c36650692a97ef476
Jagan Teki <jagan@edgeble.ai>
2024-05-24 01:15:32 +0530

    download-artifacts: bump kernel to v2024.05-6TOPS
    Signed-off-by: Jagan Teki <jagan@edgeble.ai>

--- download-artifacts.sh
+++ download-artifacts.sh
@@ -13,7 +13,7 @@ wget --no-verbose --show-progress -P prebuilt "https://github.com/openaia/u-boot
 wget --no-verbose --show-progress -P prebuilt "https://github.com/openaia/u-boot/releases/download/$U_BOOT_REL/u-boot.itb"
 
 # linux
-KERNEL_REL="v2024.02-6TOPS"
+KERNEL_REL="v2024.05-6TOPS"
 echo "=== Downloading linux artifacts ==="
 wget --no-verbose --show-progress -P prebuilt "https://github.com/edgeble/kernel/releases/download/$KERNEL_REL/linux.tar.xz"
 tar xvf prebuilt/linux.tar.xz -C prebuilt
